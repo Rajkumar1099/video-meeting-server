c91950b751a6b44e7869deac7004b1c0640723e8
Rajkumar <rajkumar.lookatmeprint@gmail.com>
2023-11-27 19:50:08 +0530

    added backend url

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
+app.use(cors());
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -9,11 +10,10 @@ const io = require("socket.io")(server, {
 	}
 });
 
-app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get('/',cors(), (req, res) => {
 	res.send('Runnings');
 });
 
